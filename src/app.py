2af0f299d6687becc80a480e31320d896322cf3e
imyrta <igli-myrta@hotmail.com>
2023-03-05 19:11:40 +0100

    Encapsulate major functions into a class

--- src/app.py
+++ src/app.py
@@ -29,72 +29,71 @@ class InvalidJsonFile(Exception):
         super().__init__(self.message)
 
 
-def validate_config_file(path):
-    if not os.path.isfile(path):
-        return False
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
 
-    name, extension = os.path.splitext(path)
-    if extension != '.json':
-        return False
+    @staticmethod
+    def validate_config_file(path):
+        if not os.path.isfile(path):
+            return False
 
-    return True
+        name, extension = os.path.splitext(path)
+        if extension != '.json':
+            return False
 
+        return True
 
-def load_config_file(config_file):
-    if not validate_config_file(config_file):
-        raise InvalidJsonFile(f'{config_file}')
+    def load_config_file(self, config_file):
+        if not self.validate_config_file(config_file):
+            raise InvalidJsonFile(f'{config_file}')
 
-    with open(config_file) as file:
-        return json.load(file)
+        with open(config_file) as file:
+            return json.load(file)
 
+    @staticmethod
+    def validate_environment_variables():
+        if os.environ.get('MOCK_ENDPOINTS_JSON') is None:
+            raise CouldNotFindEnvironmentVariable('MOCK_ENDPOINTS_JSON')
 
-def validate_environment_variables():
-    if os.environ.get('MOCK_ENDPOINTS_JSON') is None:
-        raise CouldNotFindEnvironmentVariable('MOCK_ENDPOINTS_JSON')
-
-
-def create_endpoint(path, method, response_body, status_code=200, headers=None):
-    def endpoint(*args, **kwargs):
-        params = kwargs.copy()
-        stringify_response = json.dumps(response_body)
-        if "{{" in stringify_response and "}}" in stringify_response:
-            if len(params) > 0:
-                renderer = PathParameterRenderer(stringify_response)
-                body = renderer.render(params)
-                return jsonify(body), status_code, headers
+    @staticmethod
+    def create_endpoint(path, method, response_body, status_code=200, headers=None):
+        def endpoint(*args, **kwargs):
+            params = kwargs.copy()
+            stringify_response = json.dumps(response_body)
+            if "{{" in stringify_response and "}}" in stringify_response:
+                if len(params) > 0:
+                    renderer = PathParameterRenderer(stringify_response)
+                    body = renderer.render(params)
+                    return jsonify(body), status_code, headers
+                else:
+                    return jsonify(response_body), status_code, headers
             else:
                 return jsonify(response_body), status_code, headers
-        else:
-            return jsonify(response_body), status_code, headers
-    endpoint.__name__ = f"{method}_{path.replace('/', '_')}"
-    return endpoint
 
+        endpoint.__name__ = f"{method}_{path.replace('/', '_')}"
+        return endpoint
 
-def register_mock_endpoints(app, mock_endpoints):
-    for endpoint in mock_endpoints:
-        method = endpoint['method']
-        path = endpoint['path']
-        status_code = endpoint.get('status_code', 200)
-        headers = endpoint.get('headers', {})
-        response_body = endpoint['response_body']
+    def register_mock_endpoints(self, mock_endpoints):
+        for endpoint in mock_endpoints:
+            method = endpoint['method']
+            path = endpoint['path']
+            status_code = endpoint.get('status_code', 200)
+            headers = endpoint.get('headers', {})
+            response_body = endpoint['response_body']
 
-        endpoint_func = create_endpoint(path, method, response_body, status_code=status_code, headers=headers)
-        app.add_url_rule(path, view_func=endpoint_func, methods=[method])
+            endpoint_func = self.create_endpoint(path, method, response_body, status_code=status_code, headers=headers)
+            self.app.add_url_rule(path, view_func=endpoint_func, methods=[method])
 
-
-def main():
-    validate_environment_variables()
-
-    mock_endpoints = load_config_file(os.environ.get('MOCK_ENDPOINTS_JSON'))
-
-    app = Flask(__name__)
-
-    register_mock_endpoints(app, mock_endpoints)
-    app.run(debug=os.environ.get('DEBUG'))
+    def run(self):
+        self.validate_environment_variables()
+        self.register_mock_endpoints(self.load_config_file(os.environ.get('MOCK_ENDPOINTS_JSON')))
+        self.app.run(debug=os.environ.get('DEBUG'))
 
 
 if __name__ == '__main__':
     try:
-        main()
+        server = Server()
+        server.run()
     except KeyboardInterrupt:
         sys.exit(0)
